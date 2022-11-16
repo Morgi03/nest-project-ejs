3a0589672d1a1d8f1d837939e2ece1f3db509c19
Morgován Nimród <morgovan.nimrod@petrik.hu>
2022-11-16 13:18:28 +0100

    eddig jutottunk

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,12 @@ import { Get, Controller, Render } from '@nestjs/common';
 export class AppController {
   @Get()
   @Render('index')
-  root(): { message: string; age: number; name: string } {
-    return { message: 'Hello World!', age: 17, name: 'Morgi03' };
+  root(): { message: string; age: number; name: string; rand: number } {
+    return {
+      message: 'Hello World!',
+      age: 17,
+      name: 'Morgi03',
+      rand: Math.floor(Math.random() * 13 + 1),
+    };
   }
 }
