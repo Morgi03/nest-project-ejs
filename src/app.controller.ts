0743c21be603cbdf955a55a12d065d1e8588afc8
Morgován Nimród <morgovannimrod22@gmail.com>
2022-11-17 12:54:16 +0100

    A kiadott feladatok megoldása

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,12 +4,23 @@ import { Get, Controller, Render } from '@nestjs/common';
 export class AppController {
   @Get()
   @Render('index')
-  root(): { message: string; age: number; name: string; rand: number } {
+  root(): {
+    message: string;
+    age: number;
+    name: string;
+    rand: number;
+    rand1: number;
+    rand2: number;
+    randValues: number;
+  } {
     return {
       message: 'Hello World!',
       age: 17,
       name: 'Morgi03',
       rand: Math.floor(Math.random() * 13 + 1),
+      rand2: Math.floor(Math.random() * 100 + 1),
+      rand1: Math.floor(Math.random() * 100 + 1),
+      randValues: 0,
     };
   }
 }
